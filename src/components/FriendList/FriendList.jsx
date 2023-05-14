068ef6d1c49b29a21f4403b07b8bed773be5c786
AlinaBielska <alinkabelka5@gmail.com>
2023-05-14 21:41:38 +0200

    added files css and changed class to className

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
 import FriendListItem from '../FriendListItem/FriendListItem';
 
 const FriendList = ({ friends }) => {
     return (
-        <ul class="friend-list">
+        <ul className={css.friendList}>
             {friends.map(friend => {
                 return (
                     <FriendListItem
